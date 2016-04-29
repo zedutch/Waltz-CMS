78b4b999fbdbf4df088bc6cd97566353bb624791
Robin Arys <contact@zedutchgandalf.be>
2016-04-30 01:06:19 +0200

    Fix the CMS-Backend service
    + add a "getAllPosts" method

--- app/scripts/cms-backend.service.ts
+++ app/scripts/cms-backend.service.ts
@@ -1,10 +1,21 @@
 import {Injectable} from 'angular2/core';
+import {Http}       from 'angular2/http';
 
 @Injectable()
 export class CMSBackendService {
     URL = 'http://localhost:8080/api';
-    constructor() { }
+    _epPosts = '/posts';
 
-    api(callback) {
+    constructor(private http : Http) { }
+
+    getAllPosts(callback) {
+        var data = this.http.get(this.URL + this._epPosts);
+        data.subscribe(res => {
+            if (res.status === 200) {
+                callback(res.json());
+            } else {
+                console.error("Error retrieving all posts!", res)
+            }
+        });
     }
 }
