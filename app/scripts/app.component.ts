fc21e033e60efbe934aa77b4dbb7f45b96ca555b
Robin Arys <contact@zedutchgandalf.be>
2016-05-01 00:36:07 +0200

    Add localization

--- app/scripts/app.component.ts
+++ app/scripts/app.component.ts
@@ -1,6 +1,8 @@
 import {Component}          from 'angular2/core';
 import {OnInit}             from 'angular2/core';
 import {HTTP_PROVIDERS}     from 'angular2/http';
+import {TranslatePipe}      from 'angular2localization/angular2localization';
+import {LocaleService, LocalizationService} from 'angular2localization/angular2localization';
 
 import {CMSBackendService}  from './cms-backend.service';
 
@@ -9,8 +11,14 @@ import {PostComponent}      from './post.component';
 @Component({
     selector    : 'waltz-main',
     templateUrl : 'views/view1',
-    providers   : [HTTP_PROVIDERS, CMSBackendService],
-    directives  : [PostComponent]
+    providers   : [
+                    HTTP_PROVIDERS,
+                    CMSBackendService,
+                    LocaleService,
+                    LocalizationService
+                  ],
+    directives  : [PostComponent],
+    pipes       : [TranslatePipe]
 })
 
 export class AppComponent implements OnInit {
@@ -18,8 +26,15 @@ export class AppComponent implements OnInit {
     url;
     posts = [];
     
-    constructor(private _cmsBackendService: CMSBackendService) {
+    constructor(private _cmsBackendService: CMSBackendService,
+                public locale: LocaleService,
+                public localization: LocalizationService) {
         this.url = this._cmsBackendService.URL;
+        this.locale.addLanguage('en');
+        this.locale.addLanguage('nl');
+        this.locale.definePreferredLocale('nl', 'BE', 30);
+        this.locale.definePreferredCurrency('EUR');
+        this.localization.translationProvider('../lang/locale-');
     }
 
     ngOnInit() {
--- app/scripts/post.component.ts
+++ app/scripts/post.component.ts
@@ -1,8 +1,10 @@
-import {Component, Input} from 'angular2/core';
+import {Component, Input}              from 'angular2/core';
+import {TranslatePipe, LocaleDatePipe} from 'angular2localization/angular2localization';
 
 @Component({
     selector    : 'post',
-    templateUrl : 'components/post'
+    templateUrl : 'components/post',
+    pipes       : [TranslatePipe, LocaleDatePipe]
 })
 
 export class PostComponent {
