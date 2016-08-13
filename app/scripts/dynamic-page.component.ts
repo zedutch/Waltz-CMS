585cd5ae580b0d75be37f698a5b4bf5b6cb918a9
Robin Arys <contact@zedutchgandalf.be>
2016-08-13 02:03:05 +0200

    Update the page object when it's changed

--- app/scripts/dynamic-page.component.ts
+++ app/scripts/dynamic-page.component.ts
@@ -4,12 +4,16 @@ import {RouteSegment}       from '@angular/router';
 import {TranslatePipe}      from 'angular2localization/angular2localization';
 
 import {CMSBackendService}  from './cms-backend.service';
+import {EditorComponent}    from './editor.component';
 
 @Component({
     selector    : 'dynamic-page',
     templateUrl : '/views/dynamic-page',
     providers   : [
                     CMSBackendService
+                  ],
+    directives  : [
+                    EditorComponent
                   ],
     pipes       : [TranslatePipe]
 })
@@ -20,11 +24,17 @@ export class DynamicPageComponent implements OnInit {
     constructor(private _cmsBackendService : CMSBackendService,
                 private _currSegment       : RouteSegment) {}
 
-    ngOnInit() {
+    ngOnInit () {
         let self = this;
         let urlString = this._currSegment.getParam('urlString');
         this._cmsBackendService.getPage(urlString, function(page) {
             self.page = page;
+        });
+    }
+
+    updatePage () {
+        this._cmsBackendService.updatePage(this.page, pageUpdate => {
+            this.page = pageUpdate;
         });
     }
 }
